f1d06deda1b4e79a13f4baa6b1d8f558e9098078
Caleb Hickey <chickey@payitgov.com>
2025-06-17 20:36:19 -0500

    committing my exercise

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// so much empty
+document.getElementById("fetchButton").addEventListener("click", () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            console.log(json);
+            document.getElementById("postList").innerHTML = json.title
+        });
+        
+        
+});
+
+document.getElementById("submitButton").addEventListener("click", () => {
+    const postData = {
+        title: document.getElementById("titleInput").value,
+        body: document.getElementById("bodyInput").value,
+        userId: 1,
+        
+    }
+    
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify(postData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("data recieved");
+        document.getElementById("postList").innerHTML = `Created Post ID: ${data.id} 
+        ${title}
+        ${body}`;
+    });
+});
